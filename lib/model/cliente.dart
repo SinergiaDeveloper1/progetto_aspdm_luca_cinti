f45cefc4263d672bd1b543fb6981a76bbdc8246f
Luca Cinti <lcinti@sinergia.it>
2020-12-03 12:19:33 +0100

    Aggiunta modello

---
+++ lib/model/cliente.dart
@@ -0,0 +1,17 @@
+class Cliente {
+  final int idCliente;
+  final bool personaFisica;
+  final String nominativo;
+  final String ragioneSociale;
+  final String partitaIva;
+  final String codFiscale;
+
+  Cliente(
+    this.idCliente,
+    this.personaFisica,
+    this.nominativo,
+    this.ragioneSociale,
+    this.partitaIva,
+    this.codFiscale,
+  );
+}
---
+++ lib/model/pratica.dart
@@ -0,0 +1,19 @@
+class Pratica {
+  final int idPratica;
+  final int anno;
+  final int progressivo;
+  final String attivita;
+  final String nominativo;
+  final String ragioneSociale;
+  final String note;
+
+  Pratica(
+    this.idPratica,
+    this.anno,
+    this.progressivo,
+    this.attivita,
+    this.nominativo,
+    this.ragioneSociale,
+    this.note,
+  );
+}
